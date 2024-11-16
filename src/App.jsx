14d69f0b595c1fbf5de11f11a3edc3701043b070
meskatabusayed <meskatabusayed@gmail.com>
2024-11-16 18:13:38 +0530

    initialized

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-3xl bg-red-500'>Knoledge Cafe</h1>
+      
+    </>
+  )
+}
+
+export default App
