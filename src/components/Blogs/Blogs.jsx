9830489f54a21876ba81288917c424cc5a995f29
meskatabusayed <meskatabusayed@gmail.com>
2024-11-16 19:44:56 +0530

    blog component added

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,16 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 
 const Blogs = () => {
+    const [blogs , setBlogs] = useState([]);
     useEffect(() => {
         fetch("blogs.json")
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => setBlogs(data));
     } , [])
 
     return (
-        <div>
-            
+        <div className="md:w-2/3">
+            <h1>Blogs : {blogs.length}</h1>
+            <div>
+                {
+                    blogs?.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+                }
+            </div>
         </div>
     );
 };
