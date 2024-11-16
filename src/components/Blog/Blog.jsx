007ff520193e870920afa7ab013ec727a26f89b5
meskatabusayed <meskatabusayed@gmail.com>
2024-11-16 20:24:33 +0530

    show blog on display

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,29 @@
 
 
 const Blog = ({blog}) => {
-    console.log(blog);
+    const {id , cover , title , author, author_image , posted_date, reading_time , hashtags} = blog;
     return (
         <div>
-            
+            <img src={cover} alt="" />
+            <div className="flex justify-between mt-5">
+                <div className="flex">
+                    <img className="w-14 rounded-full object-cover" src={author_image} alt="" />
+                    <div className="ml-4">
+                        <h1 className="font-bold">{author}</h1>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <p>{reading_time} min read</p>
+                </div>
+            </div>
+            <h1 className="text-6xl mt-5">{title}</h1>
+            <p className="my-5">{
+                 
+                 hashtags?.map((hash , idx) => <span key={idx}> <a href="">{hash}</a> </span>)
+
+                }
+            </p>
         </div>
     );
 };
